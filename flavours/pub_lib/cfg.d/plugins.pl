792cc188e8e2f81c0ce14989b1ae2e962f603b3c
David R Newman (EPrints 3.4 development) <drn@ecs.soton.ac.uk>
2026-07-27 10:57:11 +0000

    Fixes #594 by removing unnecessary Import::DOI_UNIXREF config

--- flavours/pub_lib/cfg.d/plugins.pl
+++ flavours/pub_lib/cfg.d/plugins.pl
@@ -65,7 +65,6 @@ $c->{plugins}->{"Import::DOI"}->{params}->{pid} = "ourl_eprintsorg:eprintsorg";
 # own repository requirements
 $c->{plugins}->{"Import::DOI"}->{params}->{doi_field} = "id_number";
 $c->{plugins}->{"Import::DOI"}->{params}->{use_prefix} = 1;
-$c->{plugins}->{"Import::DOI_UNIXREF"}->{params} = $c->{plugins}->{"Import::DOI"}->{params};
 
 
 =head1 COPYRIGHT
